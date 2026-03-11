3f9d4ef26023bcc274f24c09889ae7c4c7a9811d
rammohan97 <rammohan.cseb@gmail.com>
2026-03-11 07:06:25 +0000

    modified

--- conditions/01-conditions.sh
+++ conditions/01-conditions.sh
@@ -3,8 +3,10 @@
 read -p "Enter a number: " NUMBER
 if [ $NUMBER -lt 10 ]; then
     echo "The number is less than 10."
+elif [ $NUMBER -eq 10 ]; then
+    echo "The number is equal to 10."
 else
-    echo "The number is greater than or equal to 10."
+    echo "The number is greater than 10."
 fi
 
 # -gt
