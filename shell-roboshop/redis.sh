81c2c917a835bbc871b98d4bd472eab48a243d7c
rammohan97 <rammohan.cseb@gmail.com>
2026-03-16 10:35:26 +0000

    creating redis file

---
+++ shell-roboshop/redis.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
+LOGS_DIR="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_DIR/${SCRIPT_NAME}.log"
+USERID=$(id -u)
+
+mkdir -p $LOGS_DIR
+echo "===== Script started executing at : $(date) ======" | tee -a $LOG_FILE
+
+# Check if the user is root
+if [ $USERID -ne 0 ]; then
+    echo -e " ${RED}You must be root to run this script.${RESET}" | tee -a $LOG_FILE
+    exit 1
+fi
+
+# Create a function to check the validation of the installation process
+VALIDATE() {
+if [ $1 -eq 0 ]; then
+    echo -e " $2 ${GREEN} Success ${RESET}" | tee -a $LOG_FILE
+else
+    echo -e " $2 ${RED}  Failed  ${RESET}" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+# Disable module redis
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default module redis"
+
+# Enabling redis
+dnf module enable redis -y &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+
+# Install redis
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis"
+
+# Update listen address
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote Connections to redis"
+
+# Enable and Start redis services
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting redis"
