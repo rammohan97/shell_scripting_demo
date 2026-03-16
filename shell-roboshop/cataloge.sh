f249ef5d5defaea375586f28faa5bbf5ac6bda5c
Rammohan <81279482+rammohan97@users.noreply.github.com>
2026-03-16 13:04:46 +0530

    Quote mongodb host variable in echo command

--- shell-roboshop/cataloge.sh
+++ shell-roboshop/cataloge.sh
@@ -32,7 +32,7 @@ fi
 
 # Getting mongodb PrivateIP address and updating it in catalogue.service
 MONGODB_HOST=$(/usr/local/bin/aws ec2 describe-instances --filters "Name=tag:Name,Values=mongodb" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)
-echo $MONGODB_HOST
+echo "$MONGODB_HOST"
 sed -i "s|mongodb://.*:27017|mongodb://$MONGODB_HOST:27017|g" catalogue.service
 VALIDATE $? "Updating mongodb PrivateIP address in catalogue service file"
 
